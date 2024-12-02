f018fa682a22172cbae52c30be50ae637ba68df7
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2024-12-02 16:25:54 -0300

    Academia CRUD basico

---
+++ academia/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+
+from academia.models import Academia
+
+
+class AcademiaSerializer(ModelSerializer):
+
+    class Meta:
+        model = Academia
+        fields = "__all__"
