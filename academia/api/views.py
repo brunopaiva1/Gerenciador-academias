8c4d837c0dab65e5e24bb996ec52dcb024286257
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-12-18 15:19:16 -0300

    Revert "<feat>: Adição dos tratamentos de exerções dos views da API"
    This reverts commit d06deb2a42a73b63a70abc7a1ad141488402185d.

--- academia/api/views.py
+++ academia/api/views.py
@@ -2,10 +2,9 @@
 views da academia
 """
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
-from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from academia.api.serializers import AcademiaSerializer
 from academia.models import Academia
@@ -13,47 +12,9 @@ from academia.models import Academia
 
 class AcademiaViewSet(ModelViewSet):
     """
-    ViewSet para operações de CRUD de Academias.
+    Criação da view Academia
     """
 
     serializer_class = AcademiaSerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Academia.objects.all()
-
-    def list(self, request, *args, **kwargs):
-        try:
-            return super().list(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao listar academias.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def retrieve(self, request, *args, **kwargs):
-        try:
-            return super().retrieve(request, *args, **kwargs)
-        except ObjectDoesNotExist:
-            return Response({'erro': 'Academia não encontrada.'},
-                            status=status.HTTP_404_NOT_FOUND)
-        except Exception:
-            return Response({'erro': 'Erro ao buscar academia.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def create(self, request, *args, **kwargs):
-        try:
-            return super().create(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao criar academia.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def update(self, request, *args, **kwargs):
-        try:
-            return super().update(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao atualizar academia.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def destroy(self, request, *args, **kwargs):
-        try:
-            return super().destroy(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao deletar academia.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- users/api/views.py
+++ users/api/views.py
@@ -1,114 +1,43 @@
 """
 views da api do usuario
 """
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
-from django.core.exceptions import ObjectDoesNotExist
-
 from users.api.serializers import UsuarioSerializer, ClienteSerializer
 from users.api.serializers import FuncionarioSerializer
+
 from users.models import Usuario, Cliente, Funcionario
 
 
 class UsuarioViewSet(ModelViewSet):
     """
-    ViewSet para operações de leitura e atualização de Usuários.
+    Criação da view de Usuario
     """
+
     serializer_class = UsuarioSerializer
     permission_classes = [AllowAny]
     queryset = Usuario.objects.all()
-    http_method_names = ['get', 'put']
-
-    def list(self, request, *args, **kwargs):
-        try:
-            return super().list(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao listar usuários.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def retrieve(self, request, *args, **kwargs):
-        try:
-            return super().retrieve(request, *args, **kwargs)
-        except ObjectDoesNotExist:
-            return Response({'erro': 'Usuário não encontrado.'},
-                            status=status.HTTP_404_NOT_FOUND)
-        except Exception:
-            return Response({'erro': 'Erro ao buscar usuário.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def update(self, request, *args, **kwargs):
-        try:
-            return super().update(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao atualizar usuário.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    http_method_names = ["get", "put"]
 
 
 class FuncionarioViewSet(ModelViewSet):
     """
-    ViewSet para operações de CRUD de Funcionários.
+    Criação da view de Funcionario
     """
+
     serializer_class = FuncionarioSerializer
     permission_classes = [IsAuthenticated]
     queryset = Funcionario.objects.all()
-    filterset_fields = ['cargo', 'ativo', 'academia']
-
-    def list(self, request, *args, **kwargs):
-        try:
-            return super().list(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao listar funcionários.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def retrieve(self, request, *args, **kwargs):
-        try:
-            return super().retrieve(request, *args, **kwargs)
-        except ObjectDoesNotExist:
-            return Response({'erro': 'Funcionário não encontrado.'},
-                            status=status.HTTP_404_NOT_FOUND)
-        except Exception:
-            return Response({'erro': 'Erro ao buscar funcionário.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def create(self, request, *args, **kwargs):
-        try:
-            return super().create(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao criar funcionário.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    filterset_fields = ["cargo", "ativo", "academia"]
 
 
 class ClienteViewSet(ModelViewSet):
     """
-    ViewSet para operações de CRUD de Clientes.
+    Criação da view de Cliente
     """
+
     serializer_class = ClienteSerializer
     permission_classes = [IsAuthenticated]
     queryset = Cliente.objects.all()
-    filterset_fields = ['plano', 'ativo', 'academia']
-
-    def list(self, request, *args, **kwargs):
-        try:
-            return super().list(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao listar clientes.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def retrieve(self, request, *args, **kwargs):
-        try:
-            return super().retrieve(request, *args, **kwargs)
-        except ObjectDoesNotExist:
-            return Response({'erro': 'Cliente não encontrado.'},
-                            status=status.HTTP_404_NOT_FOUND)
-        except Exception:
-            return Response({'erro': 'Erro ao buscar cliente.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def create(self, request, *args, **kwargs):
-        try:
-            return super().create(request, *args, **kwargs)
-        except Exception:
-            return Response({'erro': 'Erro ao criar cliente.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    filterset_fields = ["plano", "ativo", "academia"]
