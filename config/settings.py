50e313b66b1cd699a8f7eeb3c924fb555175e9ea
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-12-18 16:17:03 -0300

    <raw>: verificação de segurança

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["bruno157.pythonanywhere.com", "127.0.0.1"]
 
